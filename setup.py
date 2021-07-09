52ce6b1554bb398f9d8318ded57e27b2d7b4fb4d
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-07-08 22:07:06 -0400

    add requirements.txt file + setup tool

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='norlab_model_training',
+    version='1.1',
+    packages=['util', 'models'],
+    url='https://norlab-ulaval.github.io',
+    author='Dominic Baril',
+    author_email='dominic.baril.1@ulaval.ca',
+    description='Deep Learning based motion dynamic model for SNOW-AutoRally project',
+    )
